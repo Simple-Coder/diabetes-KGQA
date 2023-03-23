8157a27506b3a7449137a05aae01ef93618ae6f4
xiedong <dxiesjz@163.com>
2023-03-23 22:37:06 +0800

    实体、关系初步获取

--- build_kg/build_kg_utils.py
+++ build_kg/build_kg_utils.py
@@ -2,12 +2,14 @@
 Created by xiedong
 @Date: 2023/3/22 21:59
 """
+import json
+
+from tqdm import tqdm
+import os
 
 
-class DiabetesExtractor(object):
+class DiabetesExtractor():
     def __init__(self):
-        super.__init__()
-
         # 18类节点
         self.diseases = []  # 疾病
         self.classes = []  # 疾病分期类型
@@ -46,6 +48,36 @@ class DiabetesExtractor(object):
         self.method_drug = []  # 用药方法->药品名称
         self.ade_drug = []  # 不良反应->药品名称
 
+    def extract_triples(self, data_path):
+        print("从json文件中转换抽取三元组")
+        with open(data_path, 'r', encoding='utf8') as f:
+            for line in tqdm(f.readlines()):
+                data_json = json.loads(line, strict=False)
+
+                # 处理每个句子
+                sentences = data_json['sentences']
+                for sentence in sentences:
+                    # 实体集合
+                    entities = sentence['entities']
+                    # for entity in entities:
+
+
+                    # 关系集合
+                    relations = sentence['relations']
+
+                    print()
+
 
 if __name__ == '__main__':
-    pass
+    # cur_dir = '/'.join(os.path.abspath(__file__).split('/')[:-1])
+    # data_path = os.path.join(cur_dir, './data/kg_data/1.json')
+
+    data_path = "../data/clear_data/clear.json"
+
+    extractor = DiabetesExtractor()
+    extractor.extract_triples(data_path)
+
+'''
+发现实体记录：{"Drug":"Drug","ADE":"ADE","Disease":"Disease","Pathogenesis":"Pathogenesis","Amount":"Amount","Duration":"Duration","Method":"Method","Operation":"Operation","Anatomy":"Anatomy","Reason":"Reason","Treatment":"Treatment","Test":"Test","Frequency":"Frequency","Class":"Class","Level":"Level","Test_Value":"Test_Value","Symptom":"Symptom","Test_items":"Test_items"}
+发现关系记录：{"Amount_Drug":"Amount_Drug","Duration_Drug":"Duration_Drug","Test_Disease":"Test_Disease","Method_Drug":"Method_Drug","Anatomy_Disease":"Anatomy_Disease","Operation_Disease":"Operation_Disease","ADE_Drug":"ADE_Drug","Reason_Disease":"Reason_Disease","Pathogenesis_Disease":"Pathogenesis_Disease","ADE_Disease":"ADE_Disease","Treatment_Disease":"Treatment_Disease","Drug_Disease":"Drug_Disease","Frequency_Drug":"Frequency_Drug","Test_items_Disease":"Test_items_Disease","Symptom_Disease":"Symptom_Disease","Class_Disease":"Class_Disease"}
+'''
