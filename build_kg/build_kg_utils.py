e3b37989dfd8b35d2e0890afc8a5c527f99548d5
dongxie <dongxie@sohu-inc.com>
2023-03-23 16:58:58 +0800

    15类实体关系

--- build_kg/build_kg_utils.py
+++ build_kg/build_kg_utils.py
@@ -28,6 +28,24 @@ class DiabetesExtractor(object):
         self.levels = []  # 程度
         self.duration = []  # 持续时间
 
+        # 15类实体关系
+        self.test_disease = []  # 检查方法->疾病
+        self.symptom_disease = []  # 临床表现->疾病
+        self.treatment_disease = []  # 非药治疗->疾病
+        self.drug_disease = []  # 药品名称->疾病
+        self.anatomy_disease = []  # 部位->疾病
+        self.reason_disease = []  # 病因->疾病
+        self.pathogenesis_disease = []  # 发病机制->疾病
+        self.operation_disease = []  # 手术->疾病
+        self.class_disease = []  # 分期类型->疾病
+        self.test_items_disease = []  # 检查指标->疾病
+
+        self.frequency_drug = []  # 用药频率->药品名称
+        self.duration_drug = []  # 持续时间->药品名称
+        self.amount_drug = []  # 用药剂量->药品名称
+        self.method_drug = []  # 用药方法->药品名称
+        self.ade_drug = []  # 不良反应->药品名称
+
 
 if __name__ == '__main__':
     pass
