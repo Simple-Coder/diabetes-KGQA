0ae1c69e69ec39f8a2bf28d09479df5fe2aa43dd
dong <1412834290@qq.com>
2023-04-05 21:50:47 +0800

    spider init

---
+++ spider/config.py
@@ -0,0 +1,4 @@
+"""
+Created by xiedong
+@Date: 2023/4/5 21:50
+"""
---
+++ spider/conversation.py
@@ -0,0 +1,16 @@
+"""
+Created by xiedong
+@Date: 2023/4/5 21:46
+"""
+import  requests
+
+class SpiderXywyDisease():
+    """
+    基于寻医问药网的糖尿病
+    """
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
