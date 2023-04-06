5170d41a2e01a0fbdc61476d620a27f84038e226
dong <1412834290@qq.com>
2023-04-06 21:50:03 +0800

    定义槽位：临床表现

--- config.py
+++ config.py
@@ -2,7 +2,21 @@
 Created by xiedong
 @Date: 2023/4/5 22:25
 """
-
+semantic_slot = {
+    "临床表现": {
+        "slot_list": ["Disease"],
+        "slot_values": None,
+        "cql_template": "MATCH(p:疾病)-[r:has_symptom]->(q:症状) WHERE p.name='{Disease}' RETURN q.name",
+        "reply_template": "'{Disease}' 疾病的病症表现一般是这样的：\n",
+        "ask_template": "您问的是疾病 '{Disease}' 的症状表现吗？",
+        "intent_strategy": "",
+        "deny_response": "人类的语言太难了！！"
+    },
+    "unrecognized": {
+        "slot_values": None,
+        "replay_answer": "非常抱歉，我还不知道如何回答您，我正在努力学习中~",
+    }
+}
 # 闲聊数据配置
 gossip_corpus = {
     "greet": [
