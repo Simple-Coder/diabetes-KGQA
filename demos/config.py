c179073cbb070253259ab3f432621f47636c3c2e
xiedong <dxiesjz@163.com>
2023-04-09 19:50:39 +0800

    config:意图编码

--- demos/config.py
+++ demos/config.py
@@ -3,6 +3,7 @@ Created by xiedong
 @Date: 2023/4/9 18:44
 """
 import torch
+import os
 
 
 class Args:
@@ -10,7 +11,6 @@ class Args:
     task = 'atis'
     # 数据存放路径
     data_dir = '../intent_slot_fill_demo/data'
-
 
     # 意图标签文件名
     intent_label_file = 'intent_label.txt'
@@ -36,3 +36,22 @@ class Args:
     learning_rate = 5e-5
     num_train_epochs = 10
     dropout_rate = 0.1
+
+    data_input_text_file = 'seq.in'
+    data_intent_label_file = 'label'
+    data_slot_labels_file = 'seq.out'
+
+    # 意图编码
+    intent_label_2id = {}
+    id2_intent_label = {}
+    with open(os.path.join(data_dir, task, intent_label_file), 'r') as fp:
+        intent_labels = fp.read().split('\n')
+        for i, intent in enumerate(intent_labels):
+            intent_label_2id[intent] = i
+            id2_intent_label[i] = intent
+
+
+if __name__ == '__main__':
+    args = Args()
+    print(args.intent_label_2id)
+    print(args.id2_intent_label)
