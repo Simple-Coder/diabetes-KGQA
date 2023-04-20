f5f6037178cdcedbe79d1a3f67114512f12a3621
dongxie <dongxie@sohu-inc.com>
2023-04-20 15:16:28 +0800

    处理中英文编码切分问题

--- build_model/process.py
+++ build_model/process.py
@@ -78,7 +78,8 @@ def convert_example_to_feature(ex_idx, example, tokenizer, config):
         token_label_ids = [0] + token_label_ids + [0] + [0] * (config.max_len - len(token_label_ids) - 2)
     # print(token_label_ids)
 
-    text = [i for i in text]
+    text = get_word_list(text)
+    # text = [i for i in text]
     inputs = tokenizer.encode_plus(
         text=text,
         max_length=config.max_len,
@@ -122,7 +123,32 @@ def get_features(raw_examples, tokenizer, args):
     return features
 
 
+def get_word_list(s1):
+    # 把句子按字分开，中文按字分，英文按单词，数字按空格
+    # regEx = re.compile('[\\W]*')  # 我们可以使用正则表达式来切分句子，切分的规则是除单词，数字外的任意字符串
+    regEx = re.compile('\W+')  # 我们可以使用正则表达式来切分句子，切分的规则是除单词，数字外的任意字符串
+    res = re.compile(r"([\u4e00-\u9fa5])")  # [\u4e00-\u9fa5]中文范围
+
+    p1 = regEx.split(s1.lower())
+    str1_list = []
+    for str in p1:
+        if res.split(str) == None:
+            str1_list.append(str)
+        else:
+            ret = res.split(str)
+            for ch in ret:
+                str1_list.append(ch)
+
+    list_word1 = [w for w in str1_list if len(w.strip()) > 0]  # 去掉为空的字符
+
+    return list_word1
+
+
 if __name__ == '__main__':
+    s = "12、China's Legend Holdings will split its several business arms to go public on stock markets,  the group's president Zhu Linan said on Tuesday.该集团总裁朱利安周二表示，haha中国联想控股将分拆其多个业务部门在股市上市。"
+    list_word1 = get_word_list(s)
+    print(list_word1)
+
     args = Args()
 
     texts = Processor.read_file('../data/intent_and_slot_data/train/seq.in')
