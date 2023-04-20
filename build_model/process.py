254e10468ddd7c0d920f60b30287fd151a8ad17c
dongxie <dongxie@sohu-inc.com>
2023-04-20 13:04:29 +0800

    数据预处理编码

--- build_model/process.py
+++ build_model/process.py
@@ -5,6 +5,9 @@ Created by xiedong
 import torch
 import re
 
+from config import Args
+from transformers import BertTokenizer
+
 
 class InputExample:
     def __init__(self, set_type, text, seq_label, token_label):
@@ -27,3 +30,97 @@ class InputFeature:
         self.seq_label_ids = seq_label_ids
         self.token_label_ids = token_label_ids
 
+
+class Processor:
+    @classmethod
+    def get_examples(cls, path, set_type):
+        raw_examples = []
+        with open(path, 'r', encoding='utf-8') as fp:
+            data = eval(fp.read())
+        for i, d in enumerate(data):
+            text = d['text']
+            seq_label = d['intent']
+            token_label = d['slots']
+            raw_examples.append(
+                InputExample(
+                    set_type,
+                    text,
+                    seq_label,
+                    token_label
+                )
+            )
+        return raw_examples
+
+
+def convert_example_to_feature(ex_idx, example, tokenizer, config):
+    set_type = example.set_type
+    text = example.text
+    seq_label = example.seq_label
+    token_label = example.token_label
+
+    seq_label_ids = config.seqlabel2id[seq_label]
+
+    token_label_ids = [0] * len(text)
+
+    slot_labels = []
+    for s in token_label.split():
+        # slot_labels.append(self.slot_labels.index(s) if s in self.slot_labels else self.slot_labels.index("UNK"))
+        slot_labels.append(
+            config.token_labels.index(s) if s in config.token_labels else config.token_labels.index("UNK"))
+
+    if len(token_label_ids) >= config.max_len - 2:
+        token_label_ids = [0] + token_label_ids + [0]
+    else:
+        token_label_ids = [0] + token_label_ids + [0] + [0] * (config.max_len - len(token_label_ids) - 2)
+    # print(token_label_ids)
+
+    text = [i for i in text]
+    inputs = tokenizer.encode_plus(
+        text=text,
+        max_length=config.max_len,
+        padding='max_length',
+        truncation='only_first',
+        return_attention_mask=True,
+        return_token_type_ids=True,
+    )
+
+    input_ids = torch.tensor(inputs['input_ids'], requires_grad=False)
+    attention_mask = torch.tensor(inputs['attention_mask'], requires_grad=False)
+    token_type_ids = torch.tensor(inputs['token_type_ids'], requires_grad=False)
+    seq_label_ids = torch.tensor(seq_label_ids, requires_grad=False)
+    token_label_ids = torch.tensor(token_label_ids, requires_grad=False)
+
+    if ex_idx < 3:
+        print(f'*** {set_type}_example-{ex_idx} ***')
+        print(f'text: {text}')
+        print(f'input_ids: {input_ids}')
+        print(f'attention_mask: {attention_mask}')
+        print(f'token_type_ids: {token_type_ids}')
+        print(f'seq_label_ids: {seq_label_ids}')
+        print(f'token_label_ids: {token_label_ids}')
+
+    feature = InputFeature(
+        input_ids,
+        attention_mask,
+        token_type_ids,
+        seq_label_ids,
+        token_label_ids,
+    )
+
+    return feature
+
+
+def get_features(raw_examples, tokenizer, args):
+    features = []
+    for i, example in enumerate(raw_examples):
+        feature = convert_example_to_feature(i, example, tokenizer, args)
+        features.append(feature)
+    return features
+
+
+if __name__ == '__main__':
+    args = Args()
+    raw_examples = Processor.get_examples('./data/test_process.json', 'test')
+    tokenizer = BertTokenizer.from_pretrained('./chinese-bert-wwm-ext/')
+    features = get_features(raw_examples, tokenizer, args)
+    print("")
