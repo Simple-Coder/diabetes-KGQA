deb22d0913d338a2c869bdb8206334340619780a
dongxie <dongxie@sohu-inc.com>
2023-04-20 12:54:24 +0800

    意图与槽位编码

--- build_model/config.py
+++ build_model/config.py
@@ -12,6 +12,32 @@ class Args:
     # 加载模型位置
     load_dir = './checkpoints/model.pt'
 
+    # 意图labels
+    seq_labels_path = '../data/intent_and_slot_data/intent_label.txt'
+    # 槽位labels
+    token_labels_path = '../data/intent_and_slot_data/slot_label.txt'
+
+    seqlabel2id = {}
+    id2seqlabel = {}
+    with open(seq_labels_path, 'r', encoding='utf-8') as fp:
+        seq_labels = fp.read().split('\n')
+        seq_labels = list(filter(lambda x: x != '', seq_labels))
+        for i, label in enumerate(seq_labels):
+            if label == '':
+                continue
+            seqlabel2id[label] = i
+            id2seqlabel[i] = label
+
+    tokenlabel2id = {}
+    id2tokenlabel = {}
+    with open(token_labels_path, 'r', encoding='utf-8') as fp:
+        token_labels = fp.read().split('\n')
+        for i, label in enumerate(token_labels):
+            if label == '':
+                continue
+            tokenlabel2id[label] = i
+            id2tokenlabel[i] = label
+
     # 隐层大小
     hidden_size = 768
     # 最大支持长度
@@ -24,3 +50,10 @@ class Args:
     epoch = 10
     # dropout率
     hidden_dropout_prob = 0.1
+
+
+if __name__ == '__main__':
+    args = Args()
+    print(args.seq_labels)
+    print(args.seqlabel2id)
+    print(args.tokenlabel2id)
