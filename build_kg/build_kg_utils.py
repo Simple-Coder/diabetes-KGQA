e50adf140a7663babae7cb8de0112b806502162f
dongxie <dongxie@sohu-inc.com>
2023-04-23 13:15:53 +0800

    关系指向调整

--- build_kg/build_kg_utils.py
+++ build_kg/build_kg_utils.py
@@ -137,8 +137,11 @@ class DiabetesExtractor():
                         relation_type_ = relation['relation_type']
                         head_entity_id = relation['head_entity_id']
                         tail_entity_id = relation['tail_entity_id']
-                        head_entity_name = entiDict[head_entity_id]
-                        tail_entity_name = entiDict[tail_entity_id]
+                        # head_entity_name = entiDict[head_entity_id]
+                        # tail_entity_name = entiDict[tail_entity_id]
+                        #调整关系
+                        head_entity_name = entiDict[tail_entity_id]
+                        tail_entity_name = entiDict[head_entity_id]
                         # 药品名称->疾病
                         if "Drug_Disease" == relation_type_:
                             self.drug_disease.append([head_entity_name, "Drug_Disease", tail_entity_name])
@@ -187,8 +190,8 @@ class DiabetesExtractor():
                             self.test_items_disease.append([head_entity_name, "Test_items_Disease", tail_entity_name])
                         # 临床表现->疾病
                         if "Symptom_Disease" == relation_type_:
-                            # self.symptom_disease.append([head_entity_name, "Symptom_Disease", tail_entity_name])
-                            self.symptom_disease.append([tail_entity_name, "Symptom_Disease", head_entity_name])
+                            self.symptom_disease.append([head_entity_name, "Symptom_Disease", tail_entity_name])
+                            # self.symptom_disease.append([tail_entity_name, "Symptom_Disease", head_entity_name])
                         # 不良反应->疾病
                         if "ADE_Disease" == relation_type_:
                             self.ade_disease.append([head_entity_name, "ADE_Disease", tail_entity_name])
