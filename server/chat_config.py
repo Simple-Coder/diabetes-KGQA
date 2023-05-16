185c1e55c3b3f336c166504528f9eba29abb2a7e
dongxie <dongxie@sohu-inc.com>
2023-05-16 16:43:55 +0800

    test_items_disease 槽config

--- server/chat_config.py
+++ server/chat_config.py
@@ -121,6 +121,15 @@ semantic_slot = {
         "intent_strategy": "",
         "deny_response": "您说的我有点不明白，您可以换个问法问我哦~"
     },
+    "test_items_disease": {
+        "slot_list": ["disease"],
+        "slot_values": None,
+        "cql_template": "MATCH (p:`检查指标`) -[r:Test_items_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
+        "reply_template": "'{disease}' 疾病的检查指标有：\n",
+        "ask_template": "您问的是疾病 '{disease}' 的检查指标吗？",
+        "intent_strategy": "",
+        "deny_response": "您说的我有点不明白，您可以换个问法问我哦~"
+    },
     "others": {
         "slot_values": None,
         "replay_answer": "非常抱歉，我还不知道如何回答您，我正在努力学习中~",
