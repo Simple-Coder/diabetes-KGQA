e4d9df910e7f50a3d8ad23a2bb17a66a07e40f52
dongxie <dongxie@sohu-inc.com>
2023-05-23 11:27:03 +0800

    按需引入 MyDesciption

--- web/src/main.js
+++ web/src/main.js
@@ -33,6 +33,11 @@ Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
+import {MyPanel, MyKeyValList, MyDescription} from '$ui'
+Vue.use(MyPanel)
+Vue.use(MyKeyValList)
+Vue.use(MyDescription)
+
 Vue.config.productionTip = false
 
 new Vue({
