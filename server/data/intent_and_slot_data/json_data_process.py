012e191adc3879b52d1f3d8d6828d3f8467284e5
xiedong <dxiesjz@163.com>
2023-05-28 20:37:57 +0800

    test_数据处理

--- server/data/intent_and_slot_data/json_data_process.py
+++ server/data/intent_and_slot_data/json_data_process.py
@@ -106,10 +106,16 @@ if __name__ == '__main__':
     # text_path = './test/seq.in'
     # slot_path = './test/seq.out'
 
-    intent_path = './train/label'
-    text_path = './train/seq.in'
-    slot_path = './train/seq.out'
-    todo_data_path = 'origin.json'
+    # intent_path = './train/label'
+    # text_path = './train/seq.in'
+    # slot_path = './train/seq.out'
+    # todo_data_path = 'origin.json'
+
+    intent_path = './test/label'
+    text_path = './test/seq.in'
+    slot_path = './test/seq.out'
+    todo_data_path = 'origin2.json'
+
     # 处理抓取数据
     processor = TodoDataProcessor(intent_path, text_path, slot_path, todo_data_path)
 
