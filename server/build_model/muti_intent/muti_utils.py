2b4b4cf28ca13b618ad4b2aff401f89f1a6cced5
xiedong <dxiesjz@163.com>
2023-05-28 22:02:19 +0800

    日志处理

--- server/build_model/muti_intent/muti_utils.py
+++ server/build_model/muti_intent/muti_utils.py
@@ -10,27 +10,27 @@ import json
 LOGS_DIR = "./logs"
 
 
-def setup_logger():
-    # 配置日志输出
-    logging.basicConfig(level=logging.INFO)
-
-    # 创建日志记录器
-    logger = logging.getLogger(__name__)
-
-    # 定义日志消息格式
-    log_format = "%(asctime)s - %(levelname)s - %(message)s"
-
-    # 配置日志格式
-    formatter = logging.Formatter(log_format)
-
-    # 创建日志处理器
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    # 将日志处理器添加到记录器
-    logger.addHandler(console_handler)
-
-    return logger
+# def setup_logger():
+#     # 配置日志输出
+#     logging.basicConfig(level=logging.INFO)
+#
+#     # 创建日志记录器
+#     logger = logging.getLogger(__name__)
+#
+#     # 定义日志消息格式
+#     log_format = "%(asctime)s - %(levelname)s - %(message)s"
+#
+#     # 配置日志格式
+#     formatter = logging.Formatter(log_format)
+#
+#     # 创建日志处理器
+#     console_handler = logging.StreamHandler()
+#     console_handler.setFormatter(formatter)
+#
+#     # 将日志处理器添加到记录器
+#     logger.addHandler(console_handler)
+#
+#     return logger
 
     # logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
     #                     datefmt='%m/%d/%Y %H:%M:%S',
