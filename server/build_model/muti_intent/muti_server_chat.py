15c3b970ab62252cc06ca33da6d8eca885193fa7
dongxie <dongxie@sohu-inc.com>
2023-05-29 15:10:30 +0800

    多意图场景

--- server/build_model/muti_intent/muti_server_chat.py
+++ server/build_model/muti_intent/muti_server_chat.py
@@ -3,6 +3,9 @@ Created by xiedong
 @Date: 2023/5/29 14:29
 """
 from websocket_server import WebsocketServer
+from logger_conf import my_log
+
+logger = my_log.logger
 
 # 创建 WebSocket 服务器
 server = WebsocketServer(host='127.0.0.1', port=8765)
@@ -15,24 +18,31 @@ clients = {}
 def client_connected(client, server):
     # 将新连接的客户端添加到客户端列表，并初始化上下文状态
     clients[client['id']] = {"context": None}
+    logger.info("clientId:{} 连接成功".format(client['id']))
 
 
 # 处理客户端断开连接事件
 def client_disconnected(client, server):
     # 移除客户端和上下文状态
     clients.pop(client['id'], None)
+    logger.info("clientId:{} 连接断开".format(client['id']))
 
 
 # 处理客户端消息事件
 def message_received(client, server, message):
+    logger.info("【Server】 接收到客户端clientId:{} 发来的消息:{}".format(client['id'], message))
     # 在这里可以根据接收到的消息进行意图识别和上下文处理
-    intent, context = recognize_intent(message, clients[client['id']]["context"])
+    intents, context = recognize_intents(message, clients[client['id']]["context"])
 
     # 更新客户端的上下文状态
     clients[client['id']]["context"] = context
 
     # 根据意图进行相应的处理
-    handle_intent(client, server, intent)
+    handled_intents = set()
+    for intent in intents:
+        if intent not in handled_intents:
+            handle_intent(client, server, intent)
+            handled_intents.add(intent)
 
 
 # 处理意图
@@ -67,13 +77,13 @@ def handle_default_intent(client, server):
     server.send_message(client, response)
 
 
-# 模拟意图识别和上下文处理，返回识别到的意图和更新后的上下文
-def recognize_intent(message, context):
-    # 在这里进行意图识别和上下文处理的操作，返回识别到的意图和更新后的上下文
+# 模拟意图识别和上下文处理，返回识别到的意图列表和更新后的上下文
+def recognize_intents(message, context):
+    # 在这里进行意图识别和上下文处理的操作，返回识别到的意图列表和更新后的上下文
     # 例如，可以使用预训练的模型和规则来进行意图识别和上下文处理
-    intent = "intent1"
+    intents = ["intent1", "intent2", "intent1"]
     updated_context = context
-    return intent, updated_context
+    return intents, updated_context
 
 
 # 注册事件处理函数
@@ -81,5 +91,6 @@ server.set_fn_new_client(client_connected)
 server.set_fn_client_left(client_disconnected)
 server.set_fn_message_received(message_received)
 
+logger.info("[websocket 服务端启动成功!]")
 # 启动 WebSocket 服务器
 server.run_forever()
