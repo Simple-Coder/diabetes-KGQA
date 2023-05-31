2ebdb0e2368471f1b427221833cd17bacfbd4470
dongxie <dongxie@sohu-inc.com>
2023-05-31 13:01:18 +0800

    回答策略

--- server/build_model/muti_intent/answer_handler.py
+++ server/build_model/muti_intent/answer_handler.py
@@ -4,7 +4,7 @@ Created by xiedong
 """
 from logger_conf import my_log
 from muti_chat_config import semantic_slot
-from muti_chat_modules import gossip_robot, medical_robot
+from muti_chat_modules import gossip_robot, medical_robot, gossip_corpus
 from muti_config import Args
 
 args = Args()
@@ -19,16 +19,52 @@ def handle_all_intents(client, server, user_context):
     intent_info1 = intents[0]
     intent1 = intent_info1[0]
     intent1_intensity = intent_info1[1]
-    answer1 = answer_user_query(username, intent1, intent1_intensity, all_slots, user_context)
-    logger.info("准备回答：【{}】".format(answer1))
-    server.send_message(client, answer1)
+
+    intent_info2 = intents[1]
+    intent2 = intent_info2[0]
+    intent2_intensity = intent_info2[1]
+
+    logger.info(
+        "username:【{}】,输入：【{}】识别到意图1：【{}】，意图2：【{}】，将要检查会话策略回答".format(username, user_context.get_query(), intent1,
+                                                                      intent2))
+
+    # 1、意图1=闲聊 or 意图2=闲聊 只回答一个
+    if intent1 in gossip_corpus.keys() and intent2 in gossip_corpus.keys():
+        logger.info("识别query：【{}】对应的2个意图均为闲聊".format(user_context.get_query()))
+        answer = answer_user_query(username, intent1, intent1_intensity, all_slots, user_context)
+        logger.info("准备回答：【{}】".format(answer))
+        server.send_message(client, answer)
+
+
+    # 2、意图1=诊断 && 意图2=诊断
+    elif intent1 in semantic_slot.keys() and intent2 in semantic_slot.keys():
+        logger.info("识别query：【{}】对应的2个意图均为诊断".format(user_context.get_query()))
+        answer1 = answer_user_query(username, intent1, intent1_intensity, all_slots, user_context)
+        answer2 = answer_user_query(username, intent2, intent2_intensity, all_slots, user_context)
+        # 合并2个answer
+        logger.info("准备回答：【{}】".format(answer1 + "," + answer2))
+        server.send_message(client, answer1 + "," + answer2)
+
+
+    # 3、意图1=意图2=其他
+    elif intent1 == "others" and intent2 == "others":
+        logger.info("识别query：【{}】对应的2个意图均为未知".format(user_context.get_query()))
+        answer = semantic_slot.get("others").get('replay_answer')
+        logger.info("准备回答：【{}】".format(answer))
+        server.send_message(client, answer)
+    else:
+        logger.info("识别query：【{}】,意图1:【{}】,意图2：【{}】未找到对应回答策略".format(user_context.get_query(), intent1, intent2))
+        answer = semantic_slot.get("others").get('replay_answer')
+        logger.info("准备回答：【{}】".format(answer))
+        server.send_message(client, answer)
 
 
 def answer_user_query(username, user_intent, user_intent_intensity, all_slots, user_context):
     logger.info(
-        "answer_user_query,username:【{}】input:【{}】,user_intent:【{}】,intensity:【{}】".format(username, user_intent,
-                                                                                           user_context.get_query(),
-                                                                                           user_intent_intensity))
+        "answer_user_query,username:【{}】输入:【{}】,user_intent:【{}】,intensity:【{}】".format(username,
+                                                                                        user_context.get_query(),
+                                                                                        user_intent,
+                                                                                        user_intent_intensity))
     """
     返回问题答案
     :param username: 提问人username
