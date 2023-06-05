c3b3c4fbee748f126fd5853b42bab88bb70b5115
dongxie <dongxie@sohu-inc.com>
2023-06-05 16:10:27 +0800

    log配置

--- muti_server/main.py
+++ muti_server/main.py
@@ -4,6 +4,9 @@ Created by xiedong
 """
 import argparse
 from muti_server.robot_server.robot_server import RobotWebsocketServer
+from muti_server.utils.logger_conf import my_log
+
+log = my_log.logger
 
 
 def parse_arguments():
@@ -19,4 +22,5 @@ if __name__ == "__main__":
 
     # 创建 WebSocket 服务器
     server = RobotWebsocketServer(port)
+    log.info("服务端启动成功，端口:{}".format(port))
     server.start()
