61f3817ee1ef82fb378d0115dff820b70f8faa46
dongxie <dongxie@sohu-inc.com>
2023-06-06 16:32:50 +0800

    nlu_module模块

---
+++ muti_server/nlu_module/intent_recognizer.py
@@ -0,0 +1,17 @@
+"""
+Created by xiedong
+@Date: 2023/6/5 15:32
+"""
+
+
+# 自然语言理解（NLU）模块
+class NLU:
+    def extract_intent(self, input_text):
+        # 实现意图提取的逻辑
+        intent = "default_intent"  # 假设默认意图为 "default_intent"
+        return intent
+
+    def extract_entities(self, input_text):
+        # 实现实体识别的逻辑
+        entities = {}  # 假设没有提取到实体
+        return entities
