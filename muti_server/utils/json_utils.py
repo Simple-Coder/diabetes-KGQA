22357f89cee20ec94a4cbfe3c8adbaa6fb4e4b60
xiedong <dxiesjz@163.com>
2023-06-10 10:14:44 +0800

    json包装

---
+++ muti_server/utils/json_utils.py
@@ -0,0 +1,17 @@
+"""
+Created by xiedong
+@Date: 2023/6/10 10:05
+"""
+
+import jsonpickle
+
+
+class NoTypeInfoEncoder(jsonpickle.handlers.BaseHandler):
+    def flatten(self, obj, data):
+        # 禁止打印类型信息，直接返回对象数据
+        return obj
+
+
+def json_str(obj):
+    jsonpickle.handlers.registry.register(object, NoTypeInfoEncoder)
+    return jsonpickle.encode(obj)
