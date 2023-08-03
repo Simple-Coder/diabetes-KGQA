0232aa971f903b9f703983fefe394490b452ac21
xiedong <dxiesjz@163.com>
2023-08-03 21:36:39 +0800

    相似度计算成功 test

--- server/subgraph_recall/subgraph_recall.py
+++ server/subgraph_recall/subgraph_recall.py
@@ -6,86 +6,99 @@ from py2neo import Graph
 import torch
 from transformers import BertTokenizer, BertModel
 
-# match(n:`检查指标`) -[r:Test_items_Disease] ->(q:`疾病`) where q.name contains '病' return n,r,q limit 25
 graph = Graph(
     host="127.0.0.1",
     http_port=7474,
     user="neo4j",
     password="123456")
 
+bert_dir = 'hfl/chinese-bert-wwm-ext'
 
-class RecallSubGraph:
+
+class RecallSubGraphAnswer:
     def __init__(self):
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.model = BertModel.from_pretrained('bert-base-uncased')
-
-    # 加载NER模型（假设已训练好并保存在 ner_model 变量中）
-    # ner_model = load_ner_model()
+        self.tokenizer = BertTokenizer.from_pretrained(bert_dir)
+        self.model = BertModel.from_pretrained(bert_dir)
 
     def encode_query(self, query):
         inputs = self.tokenizer.encode_plus(query, add_special_tokens=True, return_tensors='pt')
         outputs = self.model(**inputs)
-        return outputs.last_hidden_state.mean(dim=1)
+        query_embedding = outputs.last_hidden_state.mean(dim=1)  # 平均池化操作
+        return query_embedding
 
     def encode_subgraph(self, subgraph):
         node_embedding = self.model(
             **self.tokenizer(subgraph["node"], add_special_tokens=True, return_tensors='pt')).last_hidden_state.mean(
             dim=1)
-        relationship_embedding = self.model(
-            **self.tokenizer(subgraph["relationship"], add_special_tokens=True,
-                             return_tensors='pt')).last_hidden_state.mean(
-            dim=1)
-        related_node_embedding = self.model(
-            **self.tokenizer(subgraph["related_node"], add_special_tokens=True,
-                             return_tensors='pt')).last_hidden_state.mean(
-            dim=1)
-        subgraph_embedding = torch.cat([node_embedding, relationship_embedding, related_node_embedding], dim=1)
-        return subgraph_embedding
+        relationship_embedding = self.model(**self.tokenizer(subgraph["relationship"], add_special_tokens=True,
+                                                             return_tensors='pt')).last_hidden_state.mean(dim=1)
+        related_node_embedding = self.model(**self.tokenizer(subgraph["related_node"], add_special_tokens=True,
+                                                             return_tensors='pt')).last_hidden_state.mean(dim=1)
 
-    # 1、查询子图
+        return node_embedding, relationship_embedding, related_node_embedding
+
     def retrieve_subgraphs(self, query):
-        # 使用NER模型识别查询中的实体
-        # entities = ner_model(query)
         entities = ["糖尿病"]
 
-        # 根据实体查询知识图谱中的子图
         subgraphs = []
         for entity in entities:
-            # 构建查询语句，根据实体的属性查询与之相关的子图
-            cypher_query = "MATCH (n)-[r]-(m) WHERE n.name CONTAINS $entity OR m.name CONTAINS $entity RETURN n, r, m"
+            cypher_query = "MATCH (n)-[r]-(m) WHERE n.name CONTAINS $entity OR m.name CONTAINS $entity RETURN n, type(r) AS relationship, m"
             result = graph.run(cypher_query, {"entity": entity})
             for record in result:
                 subgraph = {
                     "node": record["n"]["name"],
-                    "relationship": record["r"].type,
+                    "relationship": record["relationship"],
                     "related_node": record["m"]["name"]
                 }
                 subgraphs.append(subgraph)
         return subgraphs
 
-    # 2、答案排序和输出
     def rank_answers(self, query_embedding, subgraphs):
-        ranked_subgraphs = sorted(subgraphs, key=lambda x: self.calculate_similarity(query_embedding, x), reverse=True)
+        ranked_subgraphs = sorted(subgraphs, key=lambda x: self.calculate_similarity(query_embedding, x["embedding"]), reverse=True)
         return ranked_subgraphs
 
-    def calculate_similarity(self, query_embedding, subgraph_embedding):
-        return torch.cosine_similarity(query_embedding, subgraph_embedding)
+    def calculate_similarity(self, query_embedding, subgraph_triplet):
+        node_embedding, relationship_embedding, related_node_embedding = subgraph_triplet
+
+        # 计算加权平均嵌入
+        node_weight, rel_weight, related_node_weight = 0.4, 0.3, 0.3
+        subgraph_embedding = node_weight * node_embedding + rel_weight * relationship_embedding + related_node_weight * related_node_embedding
+
+        # 计算余弦相似度
+        similarity = torch.cosine_similarity(query_embedding, subgraph_embedding, dim=1)
+        return similarity
 
     def answer_question(self, query):
         query_embedding = self.encode_query(query)
         subgraphs = self.retrieve_subgraphs(query)
-        ranked_subgraphs = self.rank_answers(query_embedding, subgraphs)
-        return ranked_subgraphs
+
+        # 遍历subgraphs，生成子图embedding和原始子图信息
+        limit = 1
+        subgraphs_with_embedding = []
+        for subgraph in subgraphs:
+            limit += 1
+            if limit == 10:
+                break
+            subgraph_embedding = self.encode_subgraph(subgraph)
+            subgraphs_with_embedding.append({"embedding": subgraph_embedding, "info": subgraph})
+
+        ranked_subgraphs = self.rank_answers(query_embedding, subgraphs_with_embedding)
+
+        # 提取排序后的子图的原始信息，即node，relationship和related_node
+        sorted_subgraphs_info = []
+        for ranked_subgraph in ranked_subgraphs:
+            subgraph_info = ranked_subgraph["info"]
+            sorted_subgraphs_info.append(subgraph_info)
+
+        return sorted_subgraphs_info
+
+
 def test():
-    # entities = ner_model(query)
     entities = ["糖尿病"]
 
-    # 根据实体查询知识图谱中的子图
     subgraphs = []
     for entity in entities:
-        # 构建查询语句，根据实体的属性查询与之相关的子图
         cypher_query = "MATCH (n)-[r]-(m) WHERE n.name CONTAINS $entity OR m.name CONTAINS $entity RETURN n, type(r) AS relationship, m"
-        # result = graph.run(cypher_query, {"entity": entity["name"]})
         result = graph.run(cypher_query, {"entity": entity})
         for record in result:
             subgraph = {
@@ -95,13 +108,16 @@ def test():
             }
             subgraphs.append(subgraph)
 
-        print("")
+    print("")
 
 
 if __name__ == '__main__':
-    pass
-    # 使用NER模型识别查询中的实体
-# query = "请问糖尿病有哪些临床表现"
-# answers = answer_question(query)
-# for answer in answers:
-#     print(f"{answer['node']} - {answer['relationship']} - {answer['related_node']}")
+    # test()
+    # print()
+
+    entities = ['糖尿病']
+    query = "请问糖尿病有哪些临床表现"
+    sub_graph = RecallSubGraphAnswer()
+    answers = sub_graph.answer_question(query)
+    for answer in answers:
+        print(f"{answer['node']} - {answer['relationship']} - {answer['related_node']}")
