d4777b8705269fed7a6bae34cfab77f67e2be884
dongxie <dongxie@sohu-inc.com>
2023-08-07 16:34:09 +0800

    ner_result

--- combine_sp_ie/nlu/entity_link.py
+++ combine_sp_ie/nlu/entity_link.py
@@ -8,6 +8,6 @@ class EntityLinkService():
     def __init__(self):
         pass
 
-    def entity_linking(self, source_entity):
+    def entity_links(self, ner_result):
         # TODO：待实现
-        return source_entity
+        return ner_result
