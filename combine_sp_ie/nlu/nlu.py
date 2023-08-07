bbe0ec09c9a6cd5bbd2ab9750d0a3150df35a9b8
dongxie <dongxie@sohu-inc.com>
2023-08-07 12:37:58 +0800

    combine: nlu Query理解模块

---
+++ combine_sp_ie/nlu/nlu.py
@@ -0,0 +1,11 @@
+"""
+Created by xiedong
+@Date: 2023/8/7 12:37
+"""
+# nlu.py
+
+def query_understanding(query):
+    main_entity = "故宫"  # 模拟直接识别出主实体
+    domain = "旅游"  # 模拟直接识别出业务领域
+    question_type = "一跳"  # 模拟直接识别出问题类型
+    return main_entity, domain, question_type
