93daa680dc93075fdda39ecc6d468758cc54a89d
dongxie <dongxie@sohu-inc.com>
2023-08-07 17:55:49 +0800

    nlu test

--- combine_sp_ie/nlu/nlu.py
+++ combine_sp_ie/nlu/nlu.py
@@ -22,6 +22,10 @@ class NLU():
         self.model_service = ModelService()
         self.entity_link_service = EntityLinkService()
 
+    def process_nlu(self, query):
+        main_entity, intent, intent_conf, question_type = self.query_understanding(query)
+        pass
+
     def query_understanding(self, query):
         # main_entity = None  # 初始化主实体
         # related_entities = []  # 初始化相关实体列表
@@ -47,19 +51,18 @@ class NLU():
         # question_type = "一跳"  # 模拟直接识别出问题类型
         return main_entity, intent, intent_conf, question_type
 
+    def relation_recognition(self, query, domain, syntax_analysis):
+        candidate_relations = ["门票", "位置", "游览", "时间", "价格"]  # 假设的候选关系
+        relation_scores = {}  # 存储关系及其分数的字典
 
-def relation_recognition(query, domain, syntax_analysis):
-    candidate_relations = ["门票", "位置", "游览", "时间", "价格"]  # 假设的候选关系
-    relation_scores = {}  # 存储关系及其分数的字典
+        # 基于依存分析等信息，对候选关系进行评分
+        for relation in candidate_relations:
+            score = calculate_relation_score(query, domain, relation, syntax_analysis)
+            relation_scores[relation] = score
 
-    # 基于依存分析等信息，对候选关系进行评分
-    for relation in candidate_relations:
-        score = calculate_relation_score(query, domain, relation, syntax_analysis)
-        relation_scores[relation] = score
-
-    # 根据分数对关系进行排序
-    sorted_relations = sorted(relation_scores.items(), key=lambda x: x[1], reverse=True)
-    return sorted_relations
+        # 根据分数对关系进行排序
+        sorted_relations = sorted(relation_scores.items(), key=lambda x: x[1], reverse=True)
+        return sorted_relations
 
 
 def calculate_relation_score(query, domain, relation, syntax_analysis):
