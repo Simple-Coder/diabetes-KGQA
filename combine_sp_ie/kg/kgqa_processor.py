18363eafae5f1993a6772df38e9675d57953449e
dongxie <dongxie@sohu-inc.com>
2023-08-08 13:13:10 +0800

    子图召回

--- combine_sp_ie/kg/kgqa_processor.py
+++ combine_sp_ie/kg/kgqa_processor.py
@@ -15,10 +15,10 @@ class KGQAProcessor():
 
     def search(self, query, constraint, main_entity, relation):
         # 1、子图召回
-        subgraphs = self.subgraph_retriever.retrieve_subgraphs(main_entity, relation)
+        subgraphs_with_embedding = self.subgraph_retriever.retrieve_subgrapsh_with_embedding(main_entity, relation)
 
         # 2、答案排序和过滤
-        single_sub_graph = self.subgraph_ranker.rank_and_filter_subgraphs(query, constraint, subgraphs)
+        single_sub_graph = self.subgraph_ranker.rank_and_filter_subgraphs(query, constraint, subgraphs_with_embedding)
 
         return single_sub_graph
 
--- combine_sp_ie/kg/subgraph_retriever.py
+++ combine_sp_ie/kg/subgraph_retriever.py
@@ -4,11 +4,14 @@ Created by xiedong
 """
 from combine_sp_ie.kg.neo4j_client import Neo4jClient
 from combine_sp_ie.utils.relation import translate_relation
+from combine_sp_ie.config.base_config import SubGraphConfig
+import combine_sp_ie.config.wrapper as wrapper
 
 
 class SubgraphRetriever:
     def __init__(self, args):
         self.args = args
+        self.subgraph_config = SubGraphConfig()
         # 初始化连接Neo4j数据库的操作，具体实现需要根据您的环境和需求来完成
         self.neo4j_client = Neo4jClient(args)
 
@@ -29,3 +32,37 @@ class SubgraphRetriever:
             }
             subgraphs.append(subgraph)
         return subgraphs
+
+    def encode_subgraph(self, subgraph):
+        """
+        子图编码
+        :param subgraph:
+        :return:
+        """
+        node_embedding = wrapper.model(
+            **wrapper.tokenizer(subgraph["node"], add_special_tokens=True, return_tensors='pt')).last_hidden_state.mean(
+            dim=1)
+        relationship_embedding = wrapper.model(**wrapper.tokenizer(subgraph["relationship"], add_special_tokens=True,
+                                                                   return_tensors='pt')).last_hidden_state.mean(dim=1)
+        related_node_embedding = wrapper.model(**wrapper.tokenizer(subgraph["related_node"], add_special_tokens=True))
+
+        return node_embedding, relationship_embedding, related_node_embedding
+
+    def subgraph_mapping(self, subgraphs):
+        # 遍历subgraphs，生成子图embedding和原始子图信息
+        subgraphs_with_embedding = []
+        for subgraph in subgraphs:
+            subgraph_embedding = self.encode_subgraph(subgraph)
+            subgraphs_with_embedding.append({"embedding": subgraph_embedding, "info": subgraph})
+
+        return subgraphs_with_embedding
+
+    def retrieve_subgrapsh_with_embedding(self, main_entity, relation):
+        # 1、查询原始子图
+        subgraphs = self.retrieve_subgraphs(main_entity, relation)
+        subgraphs = subgraphs[:self.subgraph_config.subgraph_recall_size_limit]
+
+        # 2、子团转embedding
+        subgraphs_with_embedding = self.subgraph_mapping(subgraphs)
+
+        return subgraphs_with_embedding
