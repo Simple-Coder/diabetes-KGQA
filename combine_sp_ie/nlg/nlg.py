52bce49f8b20f85afe18297a345389ed13bfc8fa
dongxie <dongxie@sohu-inc.com>
2023-08-08 16:57:03 +0800

    填充回复模板

--- combine_sp_ie/nlg/nlg.py
+++ combine_sp_ie/nlg/nlg.py
@@ -4,8 +4,29 @@ Created by xiedong
 """
 
 
-# nlg.py
+class NLG():
+    def __init__(self, args):
+        self.args = args
 
-def generate_response(main_entity, sorted_relations, subgraphs, answer):
-    # 实现自然语言生成逻辑，生成回复文本
-    pass
+    def generate_response(self, query, subgraph):
+        if not subgraph:
+            return "抱歉，未找到相关信息。"
+        # 1、根据 {子图关系} 查询回复模板
+        response_template = self.select_response_template(subgraph)
+        # 2、填充 回复模板
+        filled_response = self.fill_response_template(response_template, subgraph)
+
+        return filled_response
+
+    def select_response_template(self, subgraph):
+        if "门票" in subgraph:
+            return "故宫的门票价格是{}。"
+        elif "位置" in subgraph:
+            return "故宫位于{}。"
+        else:
+            return "关于{}的信息是{}。"
+
+    def fill_response_template(self, template, subgraph):
+        # 根据子图内容填充回复模板
+        filled_response = template.format(*subgraph[1:])  # 忽略第一个元素（主题实体）
+        return filled_response
