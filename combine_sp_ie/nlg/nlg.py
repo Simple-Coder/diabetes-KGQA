875b4b9ec56bc55688a34edd3094573f4363921c
dongxie <dongxie@sohu-inc.com>
2023-08-08 17:32:26 +0800

    响应填充

--- combine_sp_ie/nlg/nlg.py
+++ combine_sp_ie/nlg/nlg.py
@@ -2,6 +2,7 @@
 Created by xiedong
 @Date: 2023/8/7 12:43
 """
+from combine_sp_ie.config.chat_config import semantic_slot
 
 
 class NLG():
@@ -9,8 +10,10 @@ class NLG():
         self.args = args
 
     def generate_response(self, query, subgraph):
+        # check
         if not subgraph:
-            return "抱歉，未找到相关信息。"
+            return semantic_slot.get("others").get("replay_answer")
+
         # 1、根据 {子图关系} 查询回复模板
         response_template = self.select_response_template(subgraph)
         # 2、填充 回复模板
@@ -19,14 +22,17 @@ class NLG():
         return filled_response
 
     def select_response_template(self, subgraph):
-        if "门票" in subgraph:
-            return "故宫的门票价格是{}。"
-        elif "位置" in subgraph:
-            return "故宫位于{}。"
-        else:
-            return "关于{}的信息是{}。"
+        relation = subgraph[1]
+        response_info = semantic_slot.get(relation)
+        if not response_info:
+            return semantic_slot.get("others").get("replay_answer")
+
+        return response_info.get("response_template")
 
     def fill_response_template(self, template, subgraph):
         # 根据子图内容填充回复模板
-        filled_response = template.format(*subgraph[1:])  # 忽略第一个元素（主题实体）
+        if isinstance(subgraph[-1], list):
+            filled_response = template.format(", ".join(subgraph[-1]))
+        else:
+            filled_response = template.format(*subgraph[1:])  # 忽略第一个元素（主题实体）
         return filled_response
