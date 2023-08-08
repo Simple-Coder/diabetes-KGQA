58cecfe7fedc58c1e46f2ffa06b96e40eb35efd2
dongxie <dongxie@sohu-inc.com>
2023-08-08 17:27:47 +0800

    增加新的响应key

--- combine_sp_ie/config/chat_config.py
+++ combine_sp_ie/config/chat_config.py
@@ -10,6 +10,7 @@ semantic_slot = {
         "cql_template": "MATCH(p:`临床表现`)-[r:Symptom_Disease]->(q:`疾病`) where q.name='{disease}' RETURN p.name",
         "cql_template_vision": "MATCH(p:`临床表现`)-[r:Symptom_Disease]->(q:`疾病`) where q.name='{disease}' RETURN type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的病症表现一般是这样的：\n",
+        "response_template": "'{}' 疾病的病症表现一般是这样的：{}",
         "ask_template": "您问的是疾病 '{disease}' 的症状表现吗？",
         "intent_strategy": "",
         "deny_response": "人类的语言太难了！！"
@@ -20,6 +21,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`病因`) -[r:Reason_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`病因`) -[r:Reason_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的原因是：\n",
+        "response_template": "'{}' 疾病的原因是：{}",
         "ask_template": "您问的是疾病 '{disease}' 的原因吗？",
         "intent_strategy": "",
         "deny_response": "您说的我有点不明白，您可以换个问法问我哦~"
@@ -30,6 +32,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`不良反应`) -[r:ADE_Drug]->(q:`药物名称`) where q.name='{drug}' return p.name",
         "cql_template_vision": "MATCH (p:`不良反应`) -[r:ADE_Drug]->(q:`药物名称`) where q.name='{drug}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{drug}' 药物的不良反应、副作用是：\n",
+        "response_template": "'{}' 药物的不良反应、副作用是：{}",
         "ask_template": "您问的是药物 '{drug}' 的不良反应、副作用吗？",
         "intent_strategy": "",
         "deny_response": "呃，我好像没太听懂，您可以换个问法问我哦~"
@@ -40,6 +43,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`用药剂量`) -[r:Amount_Drug]->(q:`药物名称`) where q.name='{drug}' return p.name",
         "cql_template_vision": "MATCH (p:`用药剂量`) -[r:Amount_Drug]->(q:`药物名称`) where q.name='{drug}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{drug}' 药物的用药剂量是：\n",
+        "response_template": "'{}' 药物的用药剂量是：{}",
         "ask_template": "您问的是药物 '{drug}' 的用药剂量吗？",
         "intent_strategy": "",
         "deny_response": "对不起，我听起来似乎有些不太明白，您可以换个问法问我哦~"
@@ -50,6 +54,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`部位`) -[r:Anatomy_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`部位`) -[r:Anatomy_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病发病的部位有：\n",
+        "response_template": "'{}' 疾病发病的部位有：{}",
         "ask_template": "您问的是疾病 '{disease}' 发病的部位吗？",
         "intent_strategy": "",
         "deny_response": "我不太明白您的话，您可以换个问法问我哦~"
@@ -60,6 +65,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`分期类型`) -[r:Class_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`分期类型`) -[r:Class_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病分期类型是：\n",
+        "response_template": "'{}' 疾病分期类型是：{}",
         "ask_template": "您问的是疾病 '{disease}' 分期类型吗？",
         "intent_strategy": "",
         "deny_response": "我没太明白您刚才的话，有些摸不着头脑，您可以换个问法问我哦~"
@@ -70,6 +76,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`药品名称`) -[r:Drug_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`药品名称`) -[r:Drug_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": " '{disease}' 的治疗药物有以下：\n",
+        "response_template": "'{}' 的治疗药物有以下：{}",
         "ask_template": "您是想问 '{disease}' 的治疗药物有哪些吗？",
         "intent_strategy": "",
         "deny_response": "额~似乎有点不理解您说的是啥呢~~"
@@ -80,6 +87,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`用药方法`) -[r:Method_Drug]->(q:`药品名称`) where q.name='{drug}' return p.name",
         "cql_template_vision": "MATCH (p:`用药方法`) -[r:Method_Drug]->(q:`药品名称`) where q.name='{drug}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{drug}' 药物的用药方法是：\n",
+        "response_template": "'{}' 药物的用药方法是：{}",
         "ask_template": "您问的是药物 '{drug}' 用药方法吗？",
         "intent_strategy": "",
         "deny_response": "我听起来有点糊涂，您可以换个问法问我哦~"
@@ -90,6 +98,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`持续时间`) -[r:Duration_Drug]->(q:`药品名称`) where q.name='{drug}' return p.name",
         "cql_template_vision": "MATCH (p:`持续时间`) -[r:Duration_Drug]->(q:`药品名称`) where q.name='{drug}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{drug}' 药物的持续时间是：\n",
+        "response_template": "'{}' 药物的持续时间是：{}",
         "ask_template": "您问的是药物 '{drug}' 的持续时间吗？",
         "intent_strategy": "",
         "deny_response": "很抱歉，我听不太懂，您可以换个问法问我哦~"
@@ -100,6 +109,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`用药频率`) -[r:Frequency_Drug]->(q:`药品名称`) where q.name='{drug}' return p.name",
         "cql_template_vision": "MATCH (p:`用药频率`) -[r:Frequency_Drug]->(q:`药品名称`) where q.name='{drug}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{drug}' 药物的用药频率是：\n",
+        "response_template": "'{}' 药物的用药频率是：{}",
         "ask_template": "您问的是药物 '{drug}' 的用药频率吗？",
         "intent_strategy": "",
         "deny_response": "很抱歉，我无法领会您的意思，您可以换个问法问我哦~"
@@ -110,6 +120,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`手术`) -[r:operation_disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`手术`) -[r:operation_disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病需要的手术是：\n",
+        "response_template": "'{}' 疾病需要的手术是：{}",
         "ask_template": "您问的是疾病 '{disease}' 需要做什么手术吗？",
         "intent_strategy": "",
         "deny_response": "哦~我有点难以理解您所说的内容，您可以换个问法问我哦~"
@@ -120,6 +131,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`发病机制`) -[r:Pathogenesis_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`发病机制`) -[r:Pathogenesis_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的发病机制是：\n",
+        "response_template": "'{}' 疾病的发病机制是：{}",
         "ask_template": "您问的是疾病 '{disease}' 的发病机制吗？",
         "intent_strategy": "",
         "deny_response": "很抱歉，我理解起来有点困难，您可以换个问法问我哦~"
@@ -130,6 +142,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`检查方法`) -[r:Test_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`检查方法`) -[r:Test_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的检查方法有：\n",
+        "response_template": "'{}' 疾病的检查方法有：{}",
         "ask_template": "您问的是疾病 '{disease}' 的检查方法吗？",
         "intent_strategy": "",
         "deny_response": "嗯，我有点迷糊，您可以换个问法问我哦~"
@@ -140,6 +153,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`检查指标`) -[r:Test_items_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`检查指标`) -[r:Test_items_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的检查指标有：\n",
+        "response_template": "'{}' 疾病的检查指标有：{}",
         "ask_template": "您问的是疾病 '{disease}' 的检查指标吗？",
         "intent_strategy": "",
         "deny_response": "对不起，我好像没太理解您说的话，请您再换个方式表达一下哦~"
@@ -150,6 +164,7 @@ semantic_slot = {
         "cql_template": "MATCH (p:`非药治疗`) -[r:Treatment_Disease]->(q:`疾病`) where q.name='{disease}' return p.name",
         "cql_template_vision": "MATCH (p:`非药治疗`) -[r:Treatment_Disease]->(q:`疾病`) where q.name='{disease}' return type(r) AS type,id(r) as Relid,p,q,labels(p)[0] as plabel,labels(q)[0] as qlabel",
         "reply_template": "'{disease}' 疾病的非药治疗方式有：\n",
+        "response_template": "'{}' 疾病的非药治疗方式有：{}",
         "ask_template": "您问的是疾病 '{disease}' 的非药治疗方式吗？",
         "intent_strategy": "",
         "deny_response": "我不太理解您刚才所说的内容，您可以换个问法问我哦~"
