62ec4bdad43839dceae12d96b8e60afac6a13780
dongxie <dongxie@sohu-inc.com>
2023-08-11 12:21:10 +0800

    main test

--- combine_sp_ie/models/relation_matching_model.py
+++ combine_sp_ie/models/relation_matching_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from transformers import BertModel, BertTokenizer
 
+
 class RelationMatchingModel(nn.Module):
     def __init__(self, bert_dir, num_layers=3):
         super(RelationMatchingModel, self).__init__()
@@ -12,9 +13,12 @@ class RelationMatchingModel(nn.Module):
 
     def forward(self, input_queries, main_entities, candidate_relations):
         # 编码输入的查询、主实体和候选关系
-        input_queries_ids = self.tokenizer(input_queries, padding=True, truncation=True, return_tensors="pt")["input_ids"]
-        main_entities_ids = self.tokenizer(main_entities, padding=True, truncation=True, return_tensors="pt")["input_ids"]
-        candidate_relations_ids = self.tokenizer(candidate_relations, padding=True, truncation=True, return_tensors="pt")["input_ids"]
+        input_queries_ids = self.tokenizer(input_queries, padding=True, truncation=True, return_tensors="pt")[
+            "input_ids"]
+        main_entities_ids = self.tokenizer(main_entities, padding=True, truncation=True, return_tensors="pt")[
+            "input_ids"]
+        candidate_relations_ids = \
+        self.tokenizer(candidate_relations, padding=True, truncation=True, return_tensors="pt")["input_ids"]
 
         # 使用BERT编码输入的查询、主实体和候选关系
         encoded_queries = self.bert(input_queries_ids)[0]
@@ -26,24 +30,28 @@ class RelationMatchingModel(nn.Module):
 
         # 扩展主实体和候选关系的维度以匹配encoded_queries
         main_entity_embeddings_expanded = main_entity_embeddings.unsqueeze(1).expand(-1, encoded_queries.shape[1], -1)
-        relation_embeddings_expanded = relation_embeddings.unsqueeze(0).expand(main_entity_embeddings_expanded.shape[0], -1, -1)
+        relation_embeddings_expanded = relation_embeddings.unsqueeze(0).expand(main_entity_embeddings_expanded.shape[0],
+                                                                               -1, -1)
 
         # 结合查询、主实体和关系的嵌入
-        combined_representation = torch.cat((encoded_queries, main_entity_embeddings_expanded, relation_embeddings_expanded), dim=-1)
+        combined_representation = torch.cat(
+            (encoded_queries, main_entity_embeddings_expanded, relation_embeddings_expanded), dim=-1)
 
         relation_scores = self.relation_classifier(combined_representation).squeeze(-1)
         sorted_relations = sorted(zip(candidate_relations, relation_scores.tolist()), key=lambda x: x[1], reverse=True)
         # return relation_scores
         return sorted_relations
 
-# 示例使用
-bert_dir = 'D:\dev\PycharmProjects\diabetes-KGQA\server\chinese-bert-wwm-ext'
-model = RelationMatchingModel(bert_dir, num_layers=3)
 
-input_queries = ["法国的首都是什么？", "谁写了哈利波特？"]
-main_entities = ["法国", "谁"]
-candidate_relations = ["的首都是", "写了", "由谁写"]
-sorted_relations = model(input_queries, main_entities, candidate_relations)
-# print(sorted_relations)
-for relation, score in sorted_relations:
-    print(f"关系: {relation}，匹配分数: {score}")
+if __name__ == '__main__':
+    # 示例使用
+    bert_dir = 'D:\dev\PycharmProjects\diabetes-KGQA\server\chinese-bert-wwm-ext'
+    model = RelationMatchingModel(bert_dir, num_layers=3)
+
+    input_queries = ["法国的首都是什么？", "谁写了哈利波特？"]
+    main_entities = ["法国", "谁"]
+    candidate_relations = ["的首都是", "写了", "由谁写"]
+    sorted_relations = model(input_queries, main_entities, candidate_relations)
+    # print(sorted_relations)
+    for relation, score in sorted_relations:
+        print(f"关系: {relation}，匹配分数: {score}")
