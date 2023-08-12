e6469a771507daa862c0773dbff4e9f6f7b13f0b
xiedong <dxiesjz@163.com>
2023-08-12 23:07:58 +0800

    combine：新增全局配置类

--- combine_sp_ie/config/base_config.py
+++ combine_sp_ie/config/base_config.py
@@ -7,3 +7,12 @@ Created by xiedong
 class SubGraphConfig:
     subgraph_recall_size_limit = 100
     subgraph_recall_match_threshold = 0.5
+
+
+class GlobalConfig:
+    bert_dir = 'hfl/chinese-bert-wwm-ext'
+    logs_dir = '/Users/xiedong/PycharmProjects/diabetes-KGQA/combine_sp_ie/logs'
+    graph_host = '127.0.0.1'
+    graph_http_port = 7474
+    graph_user = 'neo4j'
+    graph_password = '123456'
