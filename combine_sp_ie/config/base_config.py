3aaa8506b1c76198a39841094df540a154d3a061
xiedong <dxiesjz@163.com>
2023-08-13 11:48:28 +0800

    app：新增websocket 端口

--- combine_sp_ie/config/base_config.py
+++ combine_sp_ie/config/base_config.py
@@ -2,6 +2,8 @@
 Created by xiedong
 @Date: 2023/8/5 21:33
 """
+
+
 class GlobalConfig:
     bert_dir = 'hfl/chinese-bert-wwm-ext'
     logs_dir = '/Users/xiedong/PycharmProjects/diabetes-KGQA/combine_sp_ie/logs'
@@ -9,5 +11,6 @@ class GlobalConfig:
     graph_http_port = 7474
     graph_user = 'neo4j'
     graph_password = '123456'
+    web_socket_port = 9999
     subgraph_recall_size_limit = 100
     subgraph_recall_match_threshold = 0.5
