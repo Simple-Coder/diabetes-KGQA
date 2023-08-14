0cc8ab6d401d0839967ed95b4762ee78bdc1a0e1
dongxie <dongxie@sohu-inc.com>
2023-08-14 15:13:40 +0800

    关系识别ok

--- combine_sp_ie/nlu/relation_recognize.py
+++ combine_sp_ie/nlu/relation_recognize.py
@@ -3,16 +3,18 @@ Created by xiedong
 @Date: 2023/8/8 17:36
 """
 from combine_sp_ie.models.relation_matching_model import relation_matching_model
+from combine_sp_ie.models.model_wrapper import model_service
+from combine_sp_ie.utils.relation import relations_map_service
 
 
 class RelationRecognize():
     def relation_recognition(self, semantic_info):
-        # candidate_relations = ["门票", "位置", "游览", "时间", "价格"]  # 假设的候选关系
+        # candidate_relations = relations_map_service.get_cn_relations()  # 候选关系
         # relation_scores = {}  # 存储关系及其分数的字典
-        #
-        # # 基于依存分析等信息，对候选关系进行评分
+        # #
+        # # # 基于依存分析等信息，对候选关系进行评分
         # for relation in candidate_relations:
-        #     score = self.calculate_relation_score(query, domain, relation, syntax_analysis)
+        #     score = self.calculate_relation_score(query, relation, syntax_analysis)
         #     relation_scores[relation] = score
         #
         # # 根据分数对关系进行排序
