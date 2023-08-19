33128aa6195634abbbb2deb11d89e18956c1c33e
xiedong <dxiesjz@163.com>
2023-08-19 22:33:31 +0800

    relation 多跳

--- combine_sp_ie/utils/relation.py
+++ combine_sp_ie/utils/relation.py
@@ -43,8 +43,8 @@ class Relations():
 
     def find_related_intents_en(self, intent_name):
         intent = self.find_related_intents(intent_name)
-        if not intent:
-            return None
+        if not intent or len(intent) == 0:
+            return intent_name, intent_name
         #
         intent = intent[0][0]
         translated_relation_cn, translated_relation_en = self.translate_relation(intent)
@@ -63,16 +63,18 @@ class IntentEntity:
 
 # 创建意图实体
 intents = {
-    "Symptom_Disease": IntentEntity("临床表现"),
-    "Reason_Disease": IntentEntity("病因"),
-    "ADE_Drug": IntentEntity("不良反应"),
-    "Amount_Drug": IntentEntity("用药剂量"),
-    "Anatomy_Disease": IntentEntity("部位"),
-    "Class_Disease": IntentEntity("分期类型"),
-    "Drug_Disease": IntentEntity("药品名称"),
     "Method_Drug": IntentEntity("用药方法"),
     "Duration_Drug": IntentEntity("持续时间"),
     "Frequency_Drug": IntentEntity("用药频率"),
+    "ADE_Drug": IntentEntity("不良反应"),
+    "Amount_Drug": IntentEntity("用药剂量"),
+
+    "Drug_Disease": IntentEntity("药品名称"),
+
+    "Symptom_Disease": IntentEntity("临床表现"),
+    "Reason_Disease": IntentEntity("病因"),
+    "Anatomy_Disease": IntentEntity("部位"),
+    "Class_Disease": IntentEntity("分期类型"),
     "Operation_Disease": IntentEntity("手术"),
     "Pathogenesis_Disease": IntentEntity("发病机制"),
     "Test_Disease": IntentEntity("检查方法"),
@@ -81,21 +83,11 @@ intents = {
 }
 
 # 建立意图之间的关联关系
-intents["Symptom_Disease"].add_related_intent(intents["Drug_Disease"], "临床表现->药品名称")
-intents["Reason_Disease"].add_related_intent(intents["Drug_Disease"], "病因->药品名称")
-intents["ADE_Drug"].add_related_intent(intents["Drug_Disease"], "不良反应->药品名称")
-intents["Amount_Drug"].add_related_intent(intents["Drug_Disease"], "用药剂量->药品名称")
-intents["Anatomy_Disease"].add_related_intent(intents["Drug_Disease"], "部位->药品名称")
-intents["Class_Disease"].add_related_intent(intents["Drug_Disease"], "分期类型->药品名称")
-intents["Drug_Disease"].add_related_intent(intents["Drug_Disease"], "药品名称->疾病")
-intents["Method_Drug"].add_related_intent(intents["Drug_Disease"], "用药方法->药品名称")
-intents["Duration_Drug"].add_related_intent(intents["Drug_Disease"], "持续时间->药品名称")
-intents["Frequency_Drug"].add_related_intent(intents["Drug_Disease"], "用药频率->药品名称")
-intents["Operation_Disease"].add_related_intent(intents["Drug_Disease"], "手术->药品名称")
-intents["Pathogenesis_Disease"].add_related_intent(intents["Drug_Disease"], "发病机制->药品名称")
-intents["Test_Disease"].add_related_intent(intents["Drug_Disease"], "检查方法->药品名称")
-intents["Test_items_Disease"].add_related_intent(intents["Drug_Disease"], "检查指标->药品名称")
-intents["Treatment_Disease"].add_related_intent(intents["Drug_Disease"], "非药治疗->药品名称")
+intents["Drug_Disease"].add_related_intent(intents["Method_Drug"], "用药方法->药品名称")
+intents["Drug_Disease"].add_related_intent(intents["Duration_Drug"], "持续时间->药品名称")
+intents["Drug_Disease"].add_related_intent(intents["Frequency_Drug"], "用药频率->药品名称")
+intents["Drug_Disease"].add_related_intent(intents["ADE_Drug"], "不良反应->药品名称")
+intents["Drug_Disease"].add_related_intent(intents["Amount_Drug"], "用药剂量->药品名称")
 
 
 def translate_relation(relation):
