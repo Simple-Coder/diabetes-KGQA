7aa821cfc36d6a359ccdd92482e959cf37e499f6
dongxie <dongxie@sohu-inc.com>
2023-09-22 18:13:00 +0800

    跑通

--- rl/deeplpath/sl_policy.py
+++ rl/deeplpath/sl_policy.py
@@ -24,123 +24,38 @@ class SupervisedPolicy(nn.Module):
     def __init__(self, state_dim, action_space, learning_rate=0.001):
         super(SupervisedPolicy, self).__init__()
 
-        self.state_dim = state_dim  # 状态维度
-        self.action_space = action_space  # 动作空间的大小
-
-        # 定义神经网络的层
-        self.fc1 = nn.Linear(state_dim, 64)  # 调整隐藏层大小以满足需求
-        self.fc2 = nn.Linear(64, action_space)
-
-        self.log_softmax = nn.LogSoftmax(dim=1)
-
-        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)  # Adam优化器
+        self.policy_nn = PolicyNN(state_dim, action_space)
+        self.optimizer = optim.Adam(self.policy_nn.parameters(), lr=learning_rate)
+        self.criterion = nn.NLLLoss()  # 使用负对数似然损失
 
     def forward(self, state):
-        x = torch.relu(self.fc1(state))  # 使用ReLU激活函数进行前向传播
-        action_prob = self.fc2(x)
-        return self.log_softmax(action_prob)  # 使用LogSoftmax获取动作概率的对数值
+        return self.policy_nn(state)
 
     def predict(self, state):
         with torch.no_grad():
             action_prob = self.forward(state)
-        return action_prob.exp()  # 返回动作概率的指数值
+            return action_prob.exp().numpy()
 
     def update(self, state, action):
-        self.optimizer.zero_grad()  # 梯度清零
+        self.optimizer.zero_grad()
         action_prob = self.forward(state)
-        picked_action_prob = action_prob.gather(1, action.unsqueeze(1))  # 选择与实际动作对应的概率
-        loss = -picked_action_prob.mean()  # 计算损失，采用负对数似然
-        loss.backward()  # 反向传播
-        self.optimizer.step()  # 更新模型参数
-        return loss.item()  # 返回损失值
+        # 将 action 调整为二维张量以匹配输入
+        action = action.view(-1, 1)
+        loss = self.criterion(action_prob, action.squeeze())
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
 
 
-def train():
-    torch.manual_seed(seed=0)  # 设置随机种子以确保结果的可重复性
-    policy_nn = SupervisedPolicy(state_dim, action_space)
+if __name__ == '__main__':
+    # 示例用法:
+    state_dim = 4  # 替换为实际状态维度
+    action_space = 2  # 替换为实际动作空间大小
+    policy = SupervisedPolicy(state_dim, action_space)
 
-    with open(relationPath, 'r') as f:
-        train_data = f.readlines()
+    # 更新策略
+    sample_state = torch.tensor([1.0, 2.0, 3.0, 4.0], dtype=torch.float32)
+    sample_action = torch.tensor([0], dtype=torch.int64)  # 假设只有一个动作
+    loss = policy.update(sample_state, sample_action)
 
-    num_samples = len(train_data)
-
-    if num_samples > 500:
-        num_samples = 500
-    else:
-        num_samples = num_samples
-
-    for episode in range(num_samples):
-        print("Episode %d" % episode)
-        print('Training Sample:', train_data[episode % num_samples][:-1])
-
-        env = Env(dataPath, train_data[episode % num_samples])
-        sample = train_data[episode % num_samples].split()
-
-        try:
-            good_episodes = teacher(sample[0], sample[1], 5, env, graphpath)
-        except Exception as e:
-            print('无法找到路径')
-            continue
-
-        for item in good_episodes:
-            state_batch = []
-            action_batch = []
-            for t, transition in enumerate(item):
-                state_batch.append(transition.state)
-                action_batch.append(transition.action)
-            state_batch = np.squeeze(state_batch)
-            state_batch = np.reshape(state_batch, [-1, state_dim])
-
-            loss = policy_nn.update(torch.Tensor(state_batch), torch.LongTensor(action_batch))
-            print('Loss:', loss)
-
-    torch.save(policy_nn.state_dict(), 'models/policy_supervised_' + relation + '.pt')
-    print('模型已保存')
-
-
-def test(test_episodes):
-    torch.manual_seed(0)  # 设置随机种子以确保结果的可重复性
-    policy_nn = SupervisedPolicy(state_dim, action_space)
-
-    with open(relationPath, 'r') as f:
-        test_data = f.readlines()
-
-    test_num = len(test_data)
-
-    test_data = test_data[-test_episodes:]
-    print(len(test_data))
-
-    success = 0
-
-    # 加载已经训练好的模型
-    policy_nn.load_state_dict(torch.load('models/policy_supervised_' + relation + '.pt'))
-    policy_nn.eval()  # 将模型设置为评估模式
-    print('模型已加载')
-
-    for episode in range(len(test_data)):
-        print('测试样本 %d: %s' % (episode, test_data[episode][:-1]))
-        env = Env(dataPath, test_data[episode])
-        sample = test_data[episode].split()
-        state_idx = [env.entity2id_[sample[0]], env.entity2id_[sample[1]], 0]
-
-        for t in count():
-            state_vec = env.idx_state(state_idx)
-            action_probs = policy_nn.predict(torch.Tensor(state_vec))
-            action_chosen = np.random.choice(np.arange(action_space), p=np.squeeze(action_probs.numpy()))
-            reward, new_state, done = env.interact(state_idx, action_chosen)
-
-            if done or t == max_steps_test:
-                if done:
-                    print('成功')
-                    success += 1
-                print('本轮结束\n')
-                break
-
-            state_idx = new_state
-
-    print('成功百分比:', success / test_episodes)
-
-
-if __name__ == "__main__":
-    train()
-    # test(50)
+    print(loss)
