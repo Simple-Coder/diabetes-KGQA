7e8bc25d842cb15ba240b06bc115db2f698ab414
xiedong <dxiesjz@163.com>
2023-10-28 17:09:07 +0800

    bk

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -30,230 +30,127 @@ import Layout from '@/layout'
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
-export const constantRoutes = [  {
-    path: '/redirect',
-    component: Layout,
-    hidden: true,
-    children: [
-      {
-        path: '/redirect/:path(.*)',
-        component: () => import('@/views/redirect')
+export const constantRoutes = [{
+  path: '/redirect',
+  component: Layout,
+  hidden: true,
+  children: [
+    {
+      path: '/redirect/:path(.*)',
+      component: () => import('@/views/redirect')
+    }
+  ]
+}, {
+  path: '/login',
+  component: () => import('@/views/login/index'),
+  hidden: true
+}, {
+  path: '/register',
+  component: () => import('@/views/register'),
+  hidden: true
+}, {
+  path: '/404',
+  component: () => import('@/views/404'),
+  hidden: true
+}, {
+  path: '/',
+  component: Layout,
+  redirect: '/dashboard',
+  children: [{
+    path: 'dashboard',
+    name: 'Dashboard',
+    component: () => import('@/views/dashboard/index'),
+    meta: {
+      title: 'Dashboard', icon: 'dashboard'
+    }
+  }]
+}, {
+  path: '/system',
+  component: Layout,
+  redirect: '/system',
+  name: '系统管理',
+  meta: {
+    title: '系统管理', icon: 'el-icon-s-help'
+  },
+  children: [
+    {
+      path: '/user/table',
+      name: 'user',
+      component: () => import('@/views/system/user/index'),
+      meta: {
+        title: '用户管理', icon: 'table'
       }
-    ]
-  },
-  {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-  {
-    path: '/register',
-    component: () => import('@/views/register'),
-    hidden: true
-  },
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
-
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: {title: 'Dashboard', icon: 'dashboard'}
-    }]
-  },
-  {
-    path: '/system',
-    component: Layout,
-    redirect: '/system',
-    name: '系统管理',
-    meta: {title: '系统管理', icon: 'el-icon-s-help'},
-    children: [
-       {
-        path: '/user/table',
-        name: 'user',
-        component: () => import('@/views/system/user/index'),
-        meta: {title: '用户管理', icon: 'table'}
-      },
-       {
-        path: '/notice/table',
-        name: 'user',
-        component: () => import('@/views/system/user/index'),
-        meta: {title: '通知管理', icon: 'table'}
-      },
-
-    ]
-  },
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: {title: 'Example', icon: 'el-icon-s-help'},
-    children: [
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: {title: 'Table', icon: 'table'}
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: {title: 'Tree', icon: 'tree'}
+    },
+    {
+      path: '/notice/table',
+      name: 'user',
+      component: () => import('@/views/system/user/index'),
+      meta: {
+        title: '通知管理', icon: 'table'
       }
-    ]
+    }
+  ]
+}, {
+  path: '/example',
+  component: Layout,
+  redirect: '/example/table',
+  name: 'Example',
+  meta: {
+    title: 'Example', icon: 'el-icon-s-help'
   },
-
-  // {
-  //   path: '/form',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'index',
-  //       name: 'Form',
-  //       component: () => import('@/views/form/index'),
-  //       meta: {title: 'Form', icon: 'form'}
-  //     }
-  //   ]
-  // },
-
-  // {
-  //   path: '/nested',
-  //   component: Layout,
-  //   redirect: '/nested/menu1',
-  //   name: 'Nested',
-  //   meta: {
-  //     title: 'Nested',
-  //     icon: 'nested'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'menu1',
-  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
-  //       name: 'Menu1',
-  //       meta: { title: 'Menu1' },
-  //       children: [
-  //         {
-  //           path: 'menu1-1',
-  //           component: () => import('@/views/nested/menu1/menu1-1'),
-  //           name: 'Menu1-1',
-  //           meta: { title: 'Menu1-1' }
-  //         },
-  //         {
-  //           path: 'menu1-2',
-  //           component: () => import('@/views/nested/menu1/menu1-2'),
-  //           name: 'Menu1-2',
-  //           meta: { title: 'Menu1-2' },
-  //           children: [
-  //             {
-  //               path: 'menu1-2-1',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
-  //               name: 'Menu1-2-1',
-  //               meta: { title: 'Menu1-2-1' }
-  //             },
-  //             {
-  //               path: 'menu1-2-2',
-  //               component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
-  //               name: 'Menu1-2-2',
-  //               meta: { title: 'Menu1-2-2' }
-  //             }
-  //           ]
-  //         },
-  //         {
-  //           path: 'menu1-3',
-  //           component: () => import('@/views/nested/menu1/menu1-3'),
-  //           name: 'Menu1-3',
-  //           meta: { title: 'Menu1-3' }
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       path: 'menu2',
-  //       component: () => import('@/views/nested/menu2/index'),
-  //       name: 'Menu2',
-  //       meta: { title: 'menu2' }
-  //     }
-  //   ]
-  // },
-  {
-    path: '/kg',
-    component: Layout,
-    redirect: '/kg/question',
-    name: '知识平台',
-    meta: {title: '知识平台', icon: 'el-icon-s-help'},
-    children: [
-      {
-        path: 'question',
-        name: 'question',
-        component: () => import('@/views/kg/question/index'),
-        meta: {title: '单意图问答', icon: 'table'}
-      },
-      {
-        path: 'vision',
-        name: 'vision',
-        component: () => import('@/views/kg/vision/index'),
-        meta: { title: '多意图问答', icon: 'form'}
-      },
-      //  {
-      //   path: 'manage',
-      //   name: 'manage',
-      //   component: () => import('@/views/kg/manage/kgmanage_self'),
-      //   meta: {title: '知识管理', icon: 'form'}
-      // }
-    ]
+  children: [
+    {
+      path: 'table',
+      name: 'Table',
+      component: () => import('@/views/table/index'),
+      meta: {
+        title: 'Table', icon: 'table'
+      }
+    },
+    {
+      path: 'tree',
+      name: 'Tree',
+      component: () => import('@/views/tree/index'),
+      meta: {
+        title: 'Tree', icon: 'tree'
+      }
+    }
+  ]
+}, {
+  path: '/kg',
+  component: Layout,
+  redirect: '/kg/question',
+  name: '知识平台',
+  meta: {
+    title: '知识平台', icon: 'el-icon-s-help'
   },
-
-  // {
-  //   path: '/kg',
-  //   component: Layout,
-  //   redirect: '/kg/vision',
-  //   name: '知识图谱',
-  //   meta: {title: '知识图谱', icon: 'el-icon-s-help'},
-  //   children: [
-  //     {
-  //       path: 'vision',
-  //       name: 'vision',
-  //       component: () => import('@/views/kg/vision/index'),
-  //       meta: {title: '知识图谱', icon: 'form'}
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/kg',
-  //   component: Layout,
-  //   redirect: '/kg/manage',
-  //   name: '知识管理',
-  //   meta: {title: '知识管理', icon: 'el-icon-s-help'},
-  //   children: [
-  //     {
-  //       path: 'manage',
-  //       name: 'manage',
-  //       component: () => import('@/views/kg/manage/kgmanage_self'),
-  //       meta: {title: '知识管理', icon: 'form'}
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: 'external-link',
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-  //       meta: { title: 'External Link', icon: 'link' }
-  //     }
-  //   ]
-  // },
-
-  // 404 page must be placed at the end !!!
-  {path: '*', redirect: '/404', hidden: true}
+  children: [
+    {
+      path: 'question',
+      name: 'question',
+      component: () => import('@/views/kg/question/index'),
+      meta: {
+        title: '单意图问答', icon: 'table'
+      }
+    }, {
+      path: 'vision',
+      name: 'vision',
+      component: () => import('@/views/kg/vision/index'),
+      meta: {
+        title: '多意图问答', icon: 'form'
+      }
+    }
+    //  {
+    //   path: 'manage',
+    //   name: 'manage',
+    //   component: () => import('@/views/kg/manage/kgmanage_self'),
+    //   meta: {title: '知识管理', icon: 'form'}
+    // }
+  ]
+}, {
+// 404 page must be placed at the end !!!
+  path: '*', redirect: '/404', hidden: true
+}
 ]
 
 const createRouter = () => new Router({
