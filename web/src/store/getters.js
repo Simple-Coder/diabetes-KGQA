ce4f6e161467172dde7b2ded476820ed610105f1
xiedong <dxiesjz@163.com>
2023-10-28 18:45:59 +0800

    revert

--- web/src/store/getters.js
+++ web/src/store/getters.js
@@ -3,8 +3,8 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  permissions: state => state.user.permissions,
-  permission_routes: state => state.permission.routes,
+  // permissions: state => state.user.permissions,
+  // permission_routes: state => state.permission.routes,
   name: state => state.user.name
 }
 export default getters
