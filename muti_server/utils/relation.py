3a943910eaff19a60b1a0f41d77d8740e9c5f7b8
xiedong <dxiesjz@163.com>
2023-11-21 23:02:42 +0800

    etst

--- muti_server/utils/relation.py
+++ muti_server/utils/relation.py
@@ -8,6 +8,7 @@ def translate_relation(relation):
     # 实现关系类型的翻译逻辑，你可以自行定义关系类型到中文的映射
     relation_map = {
         "Symptom_Disease": "临床表现",
+
         "Reason_Disease": "病因",
         "ADE_Drug": "不良反应",
         "Amount_Drug": "用药剂量",
@@ -24,6 +25,8 @@ def translate_relation(relation):
         "Treatment_Disease": "非药治疗",
         "Drug_Product": "生产厂商",
         "Food_Replace": "替代品",
+
+        "symptom_disease": "临床表现",
     }
 
     # 创建反向映射字典
@@ -36,7 +39,6 @@ def translate_relation(relation):
 translated_relation_cn, translated_relation_en = translate_relation("Symptom_Disease")
 print("翻译到中文:", translated_relation_cn)
 print("翻译回英文:", translated_relation_en)
-
 
 # 测试翻译到英文
 translated_relation_cn, translated_relation_en = translate_relation("替代品")
